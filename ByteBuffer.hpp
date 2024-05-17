af9279c6e0570d28099e499175edd6957c9deff4
Rene Fischer <renefischer@fischer-homenet.de>
2024-05-17 18:14:33 +0200

    insert 32 bit value

---
+++ ByteBuffer.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <array>
+
+#include "BitPosition.h"
+
+namespace ByteBuffer  {
+
+#define ROUND_UP_BYTE(_X) ((_X == 0)? 0 : (_X / 8) + ( ( _X % 8 == 0 ) ? 0 : 1)  )
+
+template <size_t Bytes>
+    class ByteBuffer {
+        public:
+            ByteBuffer() {buf.fill(0);};
+            ~ByteBuffer() = default;
+
+        void setValue(const BitPosition pos,uint32_t value,const uint8_t bitCount) {
+            BitPosition _pos = pos;
+            BitPosition max = pos;
+            if (bitCount <= 32)
+            {
+                if (_pos + bitCount < BitPosition(Bytes,0))
+                {
+                    max = _pos + bitCount;
+                }else
+                {
+                    max = BitPosition(Bytes,0);
+                }
+            }
+            
+            uint8_t idx = 0;
+            while ( _pos < max )
+            {
+                uint8_t insert = (value >> idx);
+                insert &= 1;
+                if (insert == 1)
+                {
+                    setBit(_pos);
+                }else 
+                {
+                    resetBit(_pos);
+                }
+                _pos++;
+                idx++;
+            }
+        };
+
+        void setValue(const BitPosition pos,uint8_t value,const uint8_t bitCount) {
+            BitPosition _pos = pos;
+            BitPosition max = pos;
+            if (bitCount <= 8)
+            {
+                if (_pos + bitCount < BitPosition(Bytes,0))
+                {
+                    max = _pos + bitCount;
+                }else
+                {
+                    max = BitPosition(Bytes,0);
+                }
+            }
+            
+            uint8_t idx = 0;
+            while ( _pos < max )
+            {
+                uint8_t insert = (value >> idx);
+                insert &= 1;
+                if (insert == 1)
+                {
+                    setBit(_pos);
+                }else 
+                {
+                    resetBit(_pos);
+                }
+                _pos++;
+                idx++;
+            }
+        };
+        
+        uint32_t getValue(BitPosition pos) {
+            return 0;
+        };
+        
+        void fill(uint8_t val) {buf.fill(val);};
+    
+        constexpr size_t size() {return buf.size();};
+        uint8_t* getData() {return buf.data();};
+    private:
+        void setBit(BitPosition pos) {
+            buf.at(pos.getBytePos()) |= (1 << pos.getBitPos());
+        }; 
+        void resetBit(BitPosition pos) {
+            buf.at(pos.getBytePos()) &= ~(1 << pos.getBitPos());
+        }; 
+        std::array<uint8_t, Bytes> buf;
+    };
+}
---
+++ ByteBufferTest.cpp
@@ -0,0 +1,129 @@
+#include <gtest/gtest.h>
+
+#include "ByteBuffer.hpp"
+
+/***************************************************************************************************************
+ * Constructors
+ ***************************************************************************************************************/
+
+/// @brief test if default construction is working
+/// Default construction of ByteBuffer object creates an empty array  
+TEST(ByteBuffer, DefaultConstructionOfObject_ShouldReturnBitPositionZero) {
+  ByteBuffer::ByteBuffer<1> bp;
+  uint8_t emptyBuffer[1] = {0};
+
+  uint8_t *data = bp.getData();
+  size_t size = bp.size();
+  
+  EXPECT_EQ(size,1);
+  EXPECT_EQ(*data,*emptyBuffer);
+}
+
+/// @brief test if insert one bit at BitPosition 0 is working
+/// Test if inserting one bit at BitPosition 0 in ByteBuffer object is working 
+TEST(ByteBuffer, InsertOneBitAtPositionZero_ShouldReturnByteBufferWithExactlyOneChangedBit) {
+  ByteBuffer::ByteBuffer<1> bp1;
+  uint8_t data1 = 1;
+  uint8_t compareBuffer = 0b00000001;
+
+  bp1.setValue(ByteBuffer::bitPositionZero,data1,1);
+  uint8_t *dataBuf1 = bp1.getData();
+  size_t sizeBuf1 = bp1.size();
+  
+  ASSERT_EQ(sizeBuf1,1);
+  EXPECT_EQ(*dataBuf1,compareBuffer);
+}
+
+/// @brief test if inserting one bit at given BitPosition is working
+/// Test if inserting one Bit at given BitPosition in ByteBuffer object is working 
+TEST(ByteBuffer, InsertOneBitAtGivenPosition_ShouldReturnByteBufferWithExactlyOneChangedBit) {
+  ByteBuffer::ByteBuffer<1> bp1;
+  uint8_t data1 = 1;
+  uint8_t compareBuffer = 0b00001000;
+
+  bp1.setValue(ByteBuffer::BitPosition(0,3),data1,1);
+  uint8_t *dataBuf1 = bp1.getData();
+  size_t sizeBuf1 = bp1.size();
+  
+  ASSERT_EQ(sizeBuf1,1);
+  EXPECT_EQ(*dataBuf1,compareBuffer);
+}
+
+/// @brief test if insert one bit at BitPosition 0 is working
+/// Test if inserting one bit of many at BitPosition 0 in ByteBuffer object is working 
+TEST(ByteBuffer, InsertOneBitOfManyAtPositionZero_ShouldReturnByteBufferWithExactlyOneChangedBit) {
+  ByteBuffer::ByteBuffer<1> bp1;
+  uint8_t data1 = 0b11111111;
+  uint8_t compareBuffer = 0b00000001;
+
+  bp1.setValue(ByteBuffer::bitPositionZero,data1,1);
+  uint8_t *dataBuf1 = bp1.getData();
+  size_t sizeBuf1 = bp1.size();
+  
+  ASSERT_EQ(sizeBuf1,1);
+  EXPECT_EQ(*dataBuf1,compareBuffer);
+}
+
+/// @brief test if insert more than once is working
+/// Test if inserting a bit pattern more than once returns last value 
+TEST(ByteBuffer, InsertSecondValueAtGivenPosition_ShouldReturnByteBufferWithSecondValue) {
+  ByteBuffer::ByteBuffer<1> bp1;
+  uint8_t data1 = 0b11111111;
+  uint8_t data2 = 0b00000101;
+  uint8_t compareBuffer = 0b00000101;
+
+  bp1.setValue(ByteBuffer::bitPositionZero,data1,4);
+  bp1.setValue(ByteBuffer::bitPositionZero,data2,4);
+  uint8_t *dataBuf1 = bp1.getData();
+  size_t sizeBuf1 = bp1.size();
+  
+  ASSERT_EQ(sizeBuf1,1);
+  EXPECT_EQ(*dataBuf1,compareBuffer);
+}
+
+/// @brief test if insert over byte border is working
+/// Test if inserting a bit pattern over byte border returns correct value 
+TEST(ByteBuffer, InsertValueOverByteBorder_ShouldReturnByteBufferWithCompleteValue) {
+  ByteBuffer::ByteBuffer<2> bp1;
+  uint8_t data1 = 0b11111111;
+  uint8_t data2 = 0b00000101;
+  uint16_t compareBuffer = 0b0000011111110101;
+
+  bp1.setValue(ByteBuffer::bitPositionZero,data2,4);
+  bp1.setValue(ByteBuffer::BitPosition(0,4),data1,7);
+  uint8_t *dataBuf1 = bp1.getData();
+  size_t sizeBuf1 = bp1.size();
+  
+  ASSERT_EQ(sizeBuf1,2);
+  EXPECT_EQ(memcmp(dataBuf1,&compareBuffer,sizeof(compareBuffer)),0);
+}
+
+/// @brief test if insert 32 bit value in shorter buffer is working
+/// Test if inserting a 32 bit pattern will truncated and insert in buffer 
+TEST(ByteBuffer, Insert32BitValueInShorterBuffer_ShouldReturnByteBufferWithCompleteValue) {
+  ByteBuffer::ByteBuffer<2> bp1;
+  uint32_t data = 0xabcd;
+  uint16_t compareBuffer = 0xabcd;
+
+  bp1.setValue(ByteBuffer::bitPositionZero,data,sizeof(data) * 8);
+  uint8_t *dataBuf1 = bp1.getData();
+  size_t sizeBuf1 = bp1.size();
+  
+  ASSERT_EQ(sizeBuf1,2);
+  EXPECT_EQ(memcmp(dataBuf1,&compareBuffer,sizeof(compareBuffer)),0);
+}
+
+/// @brief test if insert over byte border is working
+/// Test if inserting a bit pattern over byte border returns correct value 
+TEST(ByteBuffer, Insert32BitValueInEmptyBuffer_ShouldReturnByteBufferWithCompleteValue) {
+  ByteBuffer::ByteBuffer<4> bp1;
+  uint32_t data = 0xabcd1234;
+  uint32_t compareBuffer = 0xabcd1234;
+
+  bp1.setValue(ByteBuffer::bitPositionZero,data,sizeof(data) * 8);
+  uint8_t *dataBuf1 = bp1.getData();
+  size_t sizeBuf1 = bp1.size();
+  
+  ASSERT_EQ(sizeBuf1,4);
+  EXPECT_EQ(memcmp(dataBuf1,&compareBuffer,sizeof(compareBuffer)),0);
+}
